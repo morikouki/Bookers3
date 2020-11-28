001caf5651a2ab10be90d488879b8cbc306b48d8
vagrant <akzouksia@icloud.com>
2020-11-28 13:38:02 +0900

    [Update]relationsip

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,6 +7,8 @@ class RelationshipsController < ApplicationController
 	end
 
 	def destroy
+		current_user.unfollow(params[:user_id])
+		redirect_back(fallback_location: root_path)
 	end
 
 	def follower
