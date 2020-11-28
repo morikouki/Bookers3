ff05534596d060f4fbc4e1bdbe5394617dd80114
vagrant <akzouksia@icloud.com>
2020-11-28 13:14:01 +0900

    [Update]relationsip

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,17 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  has_many :follower, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  has_many :follower_user, through: :follower, source: :followed
+  has_many :followed, class_name: 'Relationship', foreign_key: :followed_id, dependent: :destroy
+  has_many :followed_user, through: :followed, source: :follower
+
+  def following?(user)
+    relationships.where(follower_id: user.id).exsits?
+  end
+
+
   attachment :profile_image
 
   validates :name, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:index, :create, :edit, :update, :destroy]
   end
-  resources :users
+  resources :users do
+  	resource :relationships, only: [:create, :destroy]
+  	get 'follower' => 'relationships#follower'
+  	get 'followed' => 'relationships#followed'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
