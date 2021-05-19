741c4d5f2cf1823b0387b94a9ed4406b812a3e37
vagrant <akzouksia@icloud.com>
2021-05-19 22:17:12 +0900

    sample

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   has_many :follower, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
   has_many :follower_user, through: :follower, source: :followed
+
   has_many :followed, class_name: 'Relationship', foreign_key: :followed_id, dependent: :destroy
   has_many :followed_user, through: :followed, source: :follower
 
